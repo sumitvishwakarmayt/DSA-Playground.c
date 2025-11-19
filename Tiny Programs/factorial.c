887dcd6c838706eec054e61b1b88a58dd8fc5c92
sumit <sumitvishwakarma.tech@gmail.com>
2025-11-19 21:31:39 +0530

    ⚡Cosmized factorialProgram.c

---
+++ Tiny Programs/factorial.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main()
+{
+    int num, fact = 1;
+    printf("Enter the number:");
+    scanf("%d", &num);
+    if (num == 0 || num == 1)
+    {
+        printf("The factorial of %d is 1.", num);
+    }
+    else if (num > 1)
+    {
+        while (num > 0)
+        {
+            fact = fact * num;
+            num -= 1;
+        }
+        printf("The factorial is %d", fact);
+    }
+    else
+    {
+        printf("The entered number is negative or invalid bruh! ♥");
+    }
+
+    return 0;
+}
